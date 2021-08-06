34b20f9e042d2affea93ccceb44383e4764f501c
Patrick Mota <patrickvieiramota@gmail.com>
2021-08-06 13:56:09 -0300

    Documentação automatizada com o Swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -57,6 +57,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     // Configurações de recursos estaticos (requisicoes para arquivos, js, css, imagens)
     @Override
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 
     }
 }
